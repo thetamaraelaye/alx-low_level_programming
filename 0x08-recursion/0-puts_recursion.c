3ce71bfa702d362e8b7ec99146476184cbb448ec
Tamara <tamaraelaye@gmail.com>
2023-01-04 03:48:49 -0800

    task-0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints str, followed by new line
+ * @s: string
+ * Return: 0 (Success)
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
+	
+
+
